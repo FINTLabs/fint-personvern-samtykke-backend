040934183ca95b43e91edaefc5123829c1ba8b73
Hknots <Hknots99@gmail.com>
2023-06-14 09:03:39 +0200

    add personvern resource model library

--- src/main/java/no/fintlabs/config/WebClientConfig.java
+++ src/main/java/no/fintlabs/config/WebClientConfig.java
@@ -1,68 +1,90 @@
 package no.fintlabs.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import io.netty.channel.ChannelOption;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.endpoint.OAuth2PasswordGrantRequest;
-import org.springframework.security.oauth2.client.endpoint.ReactiveOAuth2AccessTokenResponseClient;
-import org.springframework.security.oauth2.client.endpoint.WebClientReactivePasswordTokenResponseClient;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
-import org.springframework.web.reactive.function.client.ClientRequest;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.security.oauth2.client.*;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
 
+import java.time.Duration;
+import java.util.Map;
+
+@Slf4j
+@Getter
+@Setter
 @Configuration
-public class WebClientConfig {
+public class OAuthConfiguration {
+    private final AdapterProperties props;
 
-    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-    private String tokenUri;
-
-    @Value("${fint.core.oauth2.client-id}")
-    private String clientId;
-
-    @Value("${fint.core.oauth2.client-secret}")
-    private String clientSecret;
-
-    @Value("${fint.core.oauth2.scope}")
-    private String scope;
-
-    @Value("${fint.core.oauth2.username}")
-    private String username;
-
-    @Value("${fint.core.oauth2.password}")
-    private String password;
+    public OAuthConfiguration(AdapterProperties props) {
+        this.props = props;
+    }
 
     @Bean
-    public WebClient webClient() {
-        return WebClient.builder()
-                .filter(oauth2PasswordGrant())
+    public ReactiveOAuth2AuthorizedClientManager authorizedClientManager(
+            ReactiveClientRegistrationRepository clientRegistrationRepository,
+            ReactiveOAuth2AuthorizedClientService authorizedClientService) {
+
+        ReactiveOAuth2AuthorizedClientProvider authorizedClientProvider = ReactiveOAuth2AuthorizedClientProviderBuilder.builder()
+                .password()
+                .refreshToken()
+                .build();
+
+        AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager authorizedClientManager = new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(
+                clientRegistrationRepository,
+                authorizedClientService
+        );
+
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+        authorizedClientManager.setContextAttributesMapper(oAuth2AuthorizeRequest -> Mono.just(Map.of(
+                OAuth2AuthorizationContext.USERNAME_ATTRIBUTE_NAME, props.getUsername(),
+                OAuth2AuthorizationContext.PASSWORD_ATTRIBUTE_NAME, props.getPassword()
+        )));
+
+        return authorizedClientManager;
+    }
+
+    @Bean
+    public ClientHttpConnector clientHttpConnector() {
+        return new ReactorClientHttpConnector(HttpClient.create(
+                        ConnectionProvider
+                                .builder("laidback")
+                                .maxConnections(25)
+                                .pendingAcquireMaxCount(-1)
+                                .pendingAcquireTimeout(Duration.ofMinutes(15))
+                                .maxLifeTime(Duration.ofMinutes(30))
+                                .maxIdleTime(Duration.ofMinutes(5))
+                                .build())
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 900000)
+                .responseTimeout(Duration.ofMinutes(10))
+        );
+    }
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder, ReactiveOAuth2AuthorizedClientManager authorizedClientManager, ClientHttpConnector clientHttpConnector) {
+        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(-1))
+                .build();
+
+        ServerOAuth2AuthorizedClientExchangeFilterFunction oauth2Client = new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+        oauth2Client.setDefaultClientRegistrationId(props.getRegistrationId());
+
+        return builder
+                .clientConnector(clientHttpConnector)
+                .exchangeStrategies(exchangeStrategies)
+                .filter(oauth2Client)
+                .baseUrl(props.getBaseUrl())
                 .build();
     }
-
-    private ExchangeFilterFunction oauth2PasswordGrant() {
-        return ExchangeFilterFunction.ofRequestProcessor(
-                clientRequest -> {
-                    ClientRegistration clientRegistration = ClientRegistration
-                            .withRegistrationId("fint")
-                            .tokenUri(tokenUri)
-                            .clientId(clientId)
-                            .clientSecret(clientSecret)
-                            .authorizationGrantType(AuthorizationGrantType.PASSWORD)
-                            .scope(scope)
-                            .build();
-
-                    ReactiveOAuth2AccessTokenResponseClient<OAuth2PasswordGrantRequest> accessTokenResponseClient = new WebClientReactivePasswordTokenResponseClient();
-                    OAuth2PasswordGrantRequest passwordGrantRequest = new OAuth2PasswordGrantRequest(clientRegistration, username, password);
-
-                    return accessTokenResponseClient.getTokenResponse(passwordGrantRequest)
-                            .map(accessTokenResponse -> {
-                                String accessTokenValue = accessTokenResponse.getAccessToken().getTokenValue();
-                                return ClientRequest.from(clientRequest)
-                                        .headers(headers -> headers.setBearerAuth(accessTokenValue))
-                                        .build();
-                            });
-                });
-    }
-
 }
