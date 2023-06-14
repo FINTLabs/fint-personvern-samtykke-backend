43de9db069aa5b72e12c85fe08f0943d8b69a56e
Hknots <Hknots99@gmail.com>
2023-06-14 09:05:19 +0200

    adjust oauth2 configuration for WebClient

--- src/main/java/no/fintlabs/config/WebClientConfig.java
+++ src/main/java/no/fintlabs/config/WebClientConfig.java
@@ -1,35 +1,37 @@
 package no.fintlabs.config;
 
-import io.netty.channel.ChannelOption;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ClientHttpConnector;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.security.oauth2.client.*;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-import reactor.netty.http.client.HttpClient;
-import reactor.netty.resources.ConnectionProvider;
 
-import java.time.Duration;
 import java.util.Map;
 
 @Slf4j
 @Getter
 @Setter
 @Configuration
-public class OAuthConfiguration {
-    private final AdapterProperties props;
+public class WebClientConfig {
 
-    public OAuthConfiguration(AdapterProperties props) {
-        this.props = props;
-    }
+    @Value("${fint.registration-id}")
+    private String registrationId;
+
+    @Value("${fint.base-url}")
+    private String baseUrl;
+
+    @Value("${fint.core.oauth2.username}")
+    private String username;
+
+    @Value("${fint.core.oauth2.password}")
+    private String password;
 
     @Bean
     public ReactiveOAuth2AuthorizedClientManager authorizedClientManager(
@@ -48,43 +50,27 @@ public class OAuthConfiguration {
 
         authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
         authorizedClientManager.setContextAttributesMapper(oAuth2AuthorizeRequest -> Mono.just(Map.of(
-                OAuth2AuthorizationContext.USERNAME_ATTRIBUTE_NAME, props.getUsername(),
-                OAuth2AuthorizationContext.PASSWORD_ATTRIBUTE_NAME, props.getPassword()
+                OAuth2AuthorizationContext.USERNAME_ATTRIBUTE_NAME, username,
+                OAuth2AuthorizationContext.PASSWORD_ATTRIBUTE_NAME, password
         )));
 
         return authorizedClientManager;
     }
 
     @Bean
-    public ClientHttpConnector clientHttpConnector() {
-        return new ReactorClientHttpConnector(HttpClient.create(
-                        ConnectionProvider
-                                .builder("laidback")
-                                .maxConnections(25)
-                                .pendingAcquireMaxCount(-1)
-                                .pendingAcquireTimeout(Duration.ofMinutes(15))
-                                .maxLifeTime(Duration.ofMinutes(30))
-                                .maxIdleTime(Duration.ofMinutes(5))
-                                .build())
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 900000)
-                .responseTimeout(Duration.ofMinutes(10))
-        );
-    }
-
-    @Bean
-    public WebClient webClient(WebClient.Builder builder, ReactiveOAuth2AuthorizedClientManager authorizedClientManager, ClientHttpConnector clientHttpConnector) {
+    public WebClient webClient(WebClient.Builder builder, ReactiveOAuth2AuthorizedClientManager authorizedClientManager) {
         ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
                 .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(-1))
                 .build();
 
         ServerOAuth2AuthorizedClientExchangeFilterFunction oauth2Client = new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
-        oauth2Client.setDefaultClientRegistrationId(props.getRegistrationId());
+        oauth2Client.setDefaultClientRegistrationId(registrationId);
 
         return builder
-                .clientConnector(clientHttpConnector)
                 .exchangeStrategies(exchangeStrategies)
                 .filter(oauth2Client)
-                .baseUrl(props.getBaseUrl())
+                .baseUrl(baseUrl)
                 .build();
     }
+
 }
