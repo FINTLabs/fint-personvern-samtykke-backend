4b0771a198c1fcb26a886c26b677bb5f9ba9bcb5
Hknots <Hknots99@gmail.com>
2023-06-15 09:29:28 +0200

    made method public

--- src/main/java/no/fintlabs/tjeneste/TjenesteService.java
+++ src/main/java/no/fintlabs/tjeneste/TjenesteService.java
@@ -1,13 +1,12 @@
 package no.fintlabs.tjeneste;
 
 import lombok.extern.slf4j.Slf4j;
-import no.fint.model.resource.personvern.samtykke.TjenesteResource;
 import no.fint.model.resource.personvern.samtykke.TjenesteResources;
+import no.fintlabs.utils.FintUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,14 +14,16 @@ import java.util.List;
 @Service
 public class TjenesteService {
 
+    private final FintUtils fintUtils;
+
     private final WebClient webClient;
 
-    public TjenesteService(WebClient webClient) {
+    public TjenesteService(FintUtils fintUtils, WebClient webClient) {
+        this.fintUtils = fintUtils;
         this.webClient = webClient;
     }
 
-    @PostConstruct
-    private List<Tjeneste> getTjenester() {
+    public List<Tjeneste> getTjenester() {
         List<Tjeneste> tjenester = new ArrayList<>();
         getTjenesteResources().subscribe(tjenesteResources ->
                 tjenesteResources
@@ -31,24 +32,10 @@ public class TjenesteService {
                             Tjeneste tjeneste = new Tjeneste();
                             tjeneste.setId(tjenesteResource.getSystemId().getIdentifikatorverdi());
                             tjeneste.setName(tjenesteResource.getNavn());
-                            tjeneste.setListOfRelationIds(getRelationIdsFromLinks(tjenesteResource));
+                            tjeneste.setBehandlingIds(fintUtils.getRelationIdsFromLinks(tjenesteResource, "behandling"));
                             tjenester.add(tjeneste);
                         }));
         return tjenester;
-    }
-
-    private List<String> getRelationIdsFromLinks(TjenesteResource tjenesteResource) {
-        List<String> listOfIds = new ArrayList<>();
-        tjenesteResource
-                .getLinks()
-                .get("behandlingsgrunnlag")
-                .forEach(link -> {
-                    String href = link.getHref();
-                    String[] pathSegments = href.split("/");
-                    String id = pathSegments[pathSegments.length - 1];
-                    listOfIds.add(id);
-                });
-        return listOfIds;
     }
 
     private Mono<TjenesteResources> getTjenesteResources() {
