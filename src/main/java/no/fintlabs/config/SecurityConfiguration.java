755988eb59b7b6cd0fa332aa254c2f41ea99079e
Hknots <Hknots99@gmail.com>
2023-06-23 12:11:14 +0200

    remove role check

--- src/main/java/no/fintlabs/config/SecurityConfiguration.java
+++ src/main/java/no/fintlabs/config/SecurityConfiguration.java
@@ -12,32 +12,17 @@ import reactor.core.publisher.Mono;
 @Configuration
 public class SecurityConfiguration {
 
-    private static final String PERSONVERN_SAMTYKKE = "personvern_samtykke";
-    private static final String PERSONVERN_KODEVERK = "personvern_kodeverk";
-
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
                 .authorizeExchange((authorize) -> authorize
                         .pathMatchers("/**")
-                        .access(this::hasRequiredOrgIdAndRole)
+                        .permitAll()
                         .anyExchange()
                         .authenticated()
                 )
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
         return http.build();
-    }
-
-    private Mono<AuthorizationDecision> hasRequiredOrgIdAndRole(Mono<Authentication> authentication, AuthorizationContext context) {
-        String firstRole = String.format("ROLE_FINT_Client_%s", PERSONVERN_SAMTYKKE);
-        String secondRole = String.format("ROLE_FINT_Client_%s", PERSONVERN_KODEVERK);
-        return authentication.map(auth -> {
-            boolean hasFirstRole = auth.getAuthorities().stream()
-                    .anyMatch(a -> a.getAuthority().equals(firstRole));
-            boolean hasSecondRole = auth.getAuthorities().stream()
-                    .anyMatch(a -> a.getAuthority().equals(secondRole));
-            return new AuthorizationDecision(hasFirstRole && hasSecondRole);
-        });
     }
 
 }
