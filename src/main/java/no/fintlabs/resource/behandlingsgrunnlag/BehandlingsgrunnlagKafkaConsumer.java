308eb171408da509cc892a2ef45d9bc0037f49ba
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2023-08-09 13:56:11 +0200

    add debug for kafka header for behandlingsgrunnlag

--- src/main/java/no/fintlabs/resource/behandlingsgrunnlag/BehandlingsgrunnlagKafkaConsumer.java
+++ src/main/java/no/fintlabs/resource/behandlingsgrunnlag/BehandlingsgrunnlagKafkaConsumer.java
@@ -2,14 +2,17 @@ package no.fintlabs.resource.behandlingsgrunnlag;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import no.fint.model.resource.personvern.kodeverk.BehandlingsgrunnlagResource;
 import no.fintlabs.kafka.common.topic.pattern.FormattedTopicComponentPattern;
 import no.fintlabs.kafka.entity.EntityConsumerFactoryService;
 import no.fintlabs.kafka.entity.topic.EntityTopicNamePatternParameters;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 
+@Slf4j
 @RequiredArgsConstructor
 @Getter
 @Service
@@ -22,7 +25,7 @@ public class BehandlingsgrunnlagKafkaConsumer {
     private void setupConsumer() {
         entityConsumerFactoryService.createFactory(
                 BehandlingsgrunnlagResource.class,
-                consumerRecord -> processEntity(consumerRecord.value())
+                consumerRecord -> processEntity(consumerRecord)
         ).createContainer(
                 EntityTopicNamePatternParameters
                         .builder()
@@ -33,8 +36,9 @@ public class BehandlingsgrunnlagKafkaConsumer {
         );
     }
 
-    private void processEntity(BehandlingsgrunnlagResource resource) {
-        behandlingsgrunnlagService.addResource(resource);
+    private void processEntity(ConsumerRecord<String, BehandlingsgrunnlagResource> resource) {
+        log.info("Headers:" + resource.headers().toString());
+        behandlingsgrunnlagService.addResource(resource.value());
     }
 
 }
