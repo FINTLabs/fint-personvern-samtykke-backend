c1dc57c27b19082a5230e7040fa36c3b23d05474
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2023-08-10 11:47:19 +0200

    Enable webFluxSecurity

--- src/main/java/no/fintlabs/config/SecurityConfiguration.java
+++ src/main/java/no/fintlabs/config/SecurityConfiguration.java
@@ -3,13 +3,14 @@ package no.fintlabs.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authorization.AuthorizationContext;
 import reactor.core.publisher.Mono;
 
-@Configuration
+@EnableWebFluxSecurity
 public class SecurityConfiguration {
 
     @Bean
