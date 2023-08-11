8a9d55b02a32ba50565168c81ddd671600e5f94f
Trond Magnus Sevre <trond@fintlabs.no>
2023-08-11 14:15:18 +0200

    fix location header (now with files)

--- src/main/java/no/fintlabs/utils/LocationHeader.java
+++ src/main/java/no/fintlabs/utils/LocationHeader.java
@@ -1,2 +1,18 @@
-package no.fintlabs.utils;public class LocationHeader {
+package no.fintlabs.utils;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+public class LocationHeader {
+    public static URI get(String corrId, ServerHttpRequest request) {
+
+        String url = request.getURI().toString();
+
+        return UriComponentsBuilder.fromUriString(url.substring(0, url.lastIndexOf('/')))
+                .path("/status/{corrId}")
+                .buildAndExpand(corrId)
+                .toUri();
+    }
 }
