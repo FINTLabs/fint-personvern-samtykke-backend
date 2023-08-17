5af3b5ce43e153a9a39dedc274a8ac496f4bd64c
jennifer-fintlabs <jennifer@fintlabs.no>
2023-08-17 11:54:48 +0200

    Changed application properties to be a configuration

--- src/main/java/no/fintlabs/config/ApplicationProperties.java
+++ src/main/java/no/fintlabs/config/ApplicationProperties.java
@@ -2,8 +2,10 @@ package no.fintlabs.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
 @Data
+@Configuration
 public class ApplicationProperties {
     @Value("fint.samtykke-admin.base-url")
     private String baseUrl;
