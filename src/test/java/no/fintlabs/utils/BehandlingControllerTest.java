e29d57148eb1d28f14d954205d29ea0b62fcc168
Trond Magnus Sevre <76692689+trondsevre@users.noreply.github.com>
2023-08-24 12:04:24 +0200

    Add tests (#7)
    * Add test for SecurityConfiguration
    
    * create Id if not present for Behandling
    
    * fixed url name
    
    * Add Test for behandlingsmapper
    
    ---------
    
    Co-authored-by: Hknots <Hknots99@gmail.com>
    Co-authored-by: jennifer-fintlabs <jennifer@fintlabs.no>
    Co-authored-by: Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>

---
+++ src/test/java/no/fintlabs/utils/BehandlingControllerTest.java
@@ -0,0 +1,18 @@
+package no.fintlabs.utils;
+
+import no.fintlabs.resource.behandling.BehandlingController;
+import no.fintlabs.resource.behandling.BehandlingService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+public class BehandlingControllerTest {
+
+    @Mock
+    private BehandlingService behandlingService;
+
+    @Test
+    public void testUpdateState() throws Exception {
+
+    }
+}
