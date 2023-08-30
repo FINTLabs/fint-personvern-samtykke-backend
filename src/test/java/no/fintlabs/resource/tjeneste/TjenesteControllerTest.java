1c1715fd66784a86402b460169ee9b3e568b861d
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2023-08-30 12:19:41 +0200

    Add test for TjenesteService

--- src/test/java/no/fintlabs/resource/tjeneste/TjenesteControllerTest.java
+++ src/test/java/no/fintlabs/resource/tjeneste/TjenesteControllerTest.java
@@ -1,71 +1,74 @@
-package no.fintlabs.resource.tjeneste;
-
-import no.fintlabs.resource.behandling.BehandlingService;
-import no.fintlabs.resource.tjeneste.Tjeneste;
-import no.fintlabs.resource.tjeneste.TjenesteController;
-import no.fintlabs.resource.tjeneste.TjenesteService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.http.MediaType;
-import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.server.adapter.DefaultServerWebExchange;
-import spock.lang.Ignore;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-@Ignore
-public class TjenesteControllerTest {
-
-    @Mock
-    private TjenesteService tjenesteService;
-
-    @InjectMocks
-    private TjenesteController tjenesteController;
-
-    private WebTestClient webTestClient;
-
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-        webTestClient = WebTestClient.bindToController(tjenesteController).build();
-    }
-
-    @Test
-    public void testCreateTjenesteNotCreated() {
-        String orgName = "testOrg";
-        Tjeneste tjeneste = new Tjeneste(); // Assuming a default constructor or provide necessary arguments
-        String mockCorrId = "mockedCorrId";
-
-        when(tjenesteService.create(orgName, tjeneste)).thenReturn(mockCorrId);
-
-        webTestClient.post()
-                .uri("/{orgName}", orgName)
-                .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(tjeneste)
-                .exchange()
-                .expectStatus().isNotFound();
-    }
-
-    @Test
-    public void testCreateTjenesteWhenFound() {
-        String orgName = "testOrg";
-        Tjeneste tjeneste = new Tjeneste(); // Assuming a default constructor or provide necessary arguments
-        String mockCorrId = "mockedCorrId";
-
-        when(tjenesteService.create(orgName, tjeneste)).thenReturn(mockCorrId);
-
-        webTestClient.post()
-                .uri("/tjeneste/{orgName}", orgName)
-                .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(tjeneste)
-                .exchange()
-                .expectStatus().isCreated()
-                .expectHeader().valueEquals("Location", "/tjeneste/status/mockedCorrId");
-    }
-
-}
+//package no.fintlabs.resource.tjeneste;
+//
+//import no.fintlabs.resource.behandling.BehandlingService;
+//import no.fintlabs.resource.tjeneste.Tjeneste;
+//import no.fintlabs.resource.tjeneste.TjenesteController;
+//import no.fintlabs.resource.tjeneste.TjenesteService;
+//import org.junit.Ignore;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.MockitoAnnotations;
+//import org.springframework.http.MediaType;
+//import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
+//import org.springframework.test.web.reactive.server.WebTestClient;
+//import org.springframework.web.server.adapter.DefaultServerWebExchange;
+//import spock.lang.Ignore;
+//
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.Mockito.mock;
+//import static org.mockito.Mockito.when;
+//
+//
+//
+//public class TjenesteControllerTest {
+//
+//    @Mock
+//    private TjenesteService tjenesteService;
+//
+//    @InjectMocks
+//    private TjenesteController tjenesteController;
+//
+//    private WebTestClient webTestClient;
+//
+//    @BeforeEach
+//    public void setUp() {
+//        MockitoAnnotations.openMocks(this);
+//        webTestClient = WebTestClient.bindToController(tjenesteController).build();
+//    }
+//
+//    @Test
+//    public void testCreateTjenesteNotCreated() {
+//        String orgName = "testOrg";
+//        Tjeneste tjeneste = new Tjeneste(); // Assuming a default constructor or provide necessary arguments
+//        String mockCorrId = "mockedCorrId";
+//
+//        when(tjenesteService.create(orgName, tjeneste)).thenReturn(mockCorrId);
+//
+//        webTestClient.post()
+//                .uri("/{orgName}", orgName)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .bodyValue(tjeneste)
+//                .exchange()
+//                .expectStatus().isNotFound();
+//    }
+//
+//    @Test
+//    public void testCreateTjenesteWhenFound() {
+//        String orgName = "testOrg";
+//        Tjeneste tjeneste = new Tjeneste(); // Assuming a default constructor or provide necessary arguments
+//        String mockCorrId = "mockedCorrId";
+//
+//        when(tjenesteService.create(orgName, tjeneste)).thenReturn(mockCorrId);
+//
+//        webTestClient.post()
+//                .uri("/tjeneste/{orgName}", orgName)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .bodyValue(tjeneste)
+//                .exchange()
+//                .expectStatus().isCreated()
+//                .expectHeader().valueEquals("Location", "/tjeneste/status/mockedCorrId");
+//    }
+//
+//}
--- src/test/java/no/fintlabs/resource/tjeneste/TjenesteServiceTest.java
+++ src/test/java/no/fintlabs/resource/tjeneste/TjenesteServiceTest.java
@@ -1,23 +1,25 @@
 package no.fintlabs.resource.tjeneste;
 
 import no.fint.model.resource.personvern.samtykke.TjenesteResource;
+import no.fintlabs.adapter.models.OperationType;
+import no.fintlabs.adapter.models.RequestFintEvent;
 import no.fintlabs.config.ApplicationProperties;
 import no.fintlabs.utils.EventStatusService;
 import no.fintlabs.utils.KafkaProducer;
+import no.fintlabs.utils.ResourceCollection;
+import org.junit.Ignore;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import spock.lang.Ignore;
 
-import static org.codehaus.groovy.runtime.DefaultGroovyMethods.any;
-import static org.junit.jupiter.api.Assertions.*;
-import static reactor.core.publisher.Mono.when;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-@Ignore
+
 class TjenesteServiceTest {
-
-    @InjectMocks
-    private TjenesteService tjenesteService;
 
     @Mock
     private EventStatusService eventStatusService;
@@ -29,23 +31,53 @@ class TjenesteServiceTest {
     private TjenesteMapper tjenesteMapper;
 
     @Mock
-    private ApplicationProperties applicationProperties;
+    private ResourceCollection<TjenesteResource> tjenesteResources;
+
+    @InjectMocks
+    private TjenesteService tjenesteService;
+
+    @BeforeEach
+    void setUp() {
+        ApplicationProperties applicationProperties = new ApplicationProperties();
+
+        kafkaProducer = mock(KafkaProducer.class);
+        eventStatusService = mock(EventStatusService.class);
+        tjenesteMapper = mock(TjenesteMapper.class);
+        tjenesteService = new TjenesteService(eventStatusService, kafkaProducer, tjenesteMapper, applicationProperties);
+    }
+
 
     @Test
     public void testCreateTjeneste() {
+        String orgName = "orgName";
         Tjeneste tjeneste = new Tjeneste();
         tjeneste.setNavn("Test Tjeneste");
+        TjenesteResource tjenesteResource = new TjenesteResource();
 
-        when(applicationProperties.getBaseUrl()).thenReturn("http://example.com");
-        when(tjenesteMapper.toTjenesteResource(any(Tjeneste.class))).thenReturn(new TjenesteResource());
-        when(kafkaProducer.sendEvent(any(), any(), any(), any())).thenReturn(new Request FintEvent("correlationId"));
+        when(tjenesteMapper.toTjenesteResource(tjeneste)).thenReturn(new TjenesteResource());
 
-        String orgName = "orgName";
+        RequestFintEvent requestFintEvent = new RequestFintEvent();
+        requestFintEvent.setCorrId("corrId");
+        when(kafkaProducer.sendEvent(OperationType.CREATE, "tjeneste", orgName, tjenesteResource)).thenReturn(requestFintEvent);
+
         String corrId = tjenesteService.create(orgName, tjeneste);
 
-        assertNotNull(corrId);
-        verify(eventStatusService).add("correlationId");
+        assertEquals(corrId, "corrId");
+        //verify(eventStatusService.add("corrId"));
     }
 
+    @Test
+    public void testExeption() {
+        Tjeneste tjeneste = new Tjeneste();
+        tjeneste.setNavn("");
+
+        assertThrows(IllegalArgumentException.class, ()-> tjenesteService.create("", tjeneste));
+    }
+
+    @Ignore
+    @Test
+    public void testAddBehandling(){
+
+    }
 
 }
