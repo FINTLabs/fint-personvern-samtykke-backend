9809dc18a34232d2d8b0d29df5f37a285f8f2a68
Hknots <hknots99@gmail.com>
2024-09-24 10:06:58 +0200

    disable jwt

--- src/main/java/no/fintlabs/config/SecurityConfiguration.java
+++ src/main/java/no/fintlabs/config/SecurityConfiguration.java
@@ -1,27 +1,21 @@
 package no.fintlabs.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authorization.AuthorizationContext;
-import reactor.core.publisher.Mono;
 
 @EnableWebFluxSecurity
 public class SecurityConfiguration {
 
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http
-                .authorizeExchange((authorize) -> authorize
+        return http
+                .authorizeExchange(authorize -> authorize
                         .anyExchange()
-                        .authenticated()
+                        .permitAll()
                 )
-                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
-        return http.build();
+                .build();
     }
 
 }
